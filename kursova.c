f51afa25ed6d950d9c3ccff33b8b664e1d4a4404
Viktoriia Kapys <kapysva@gmail.com>
2023-06-26 18:56:50 +0300

    створення рекурсивної процедури сортування масиву, де sort == 1 - сортування за зростанням, а sort == 2 - сортування за спаданням

--- kursova.c
+++ kursova.c
@@ -12,6 +12,43 @@ void QuickS (int Array[], /* масив для сортування */
 {
     int i = Left_B; 
     int j = Right_B;
-    
+
     int middle = (Right_B+Left_B)/2;
+    int x = Array [middle];
+    int w ;
+    do
+    {
+        if (sort == 1) {
+        while (Array[i]<x)
+        { i++ ; }
+
+        while (x<Array[j])
+        { j-- ; }
+        }
+
+        if (sort == 2) {
+        while (Array[i]>x)
+        { i++ ; }
+
+        while (x>Array[j])
+        { j-- ; }
+        }
+
+        if (i<=j) 
+        {
+           w = Array [i]; 
+           Array [i] = Array [j] ;
+           Array [j] = w ;
+           
+           i++ ;
+           j-- ;
+        }
+    }
+    while (i<j) ;
+    
+    if (Left_B<j) 
+    { QuickS (Array, Count_Q, Left_B, j, sort); } 
+    
+    if (i<Right_B)
+    { QuickS (Array, Count_Q, i, Right_B, sort);}
 }
